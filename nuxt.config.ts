cee56a8d3c3c79bad2b8f79f11cd9f3446046e79
Matt Stanley <Mattstanley2389@gmail.com>
2025-02-25 22:25:45 -0800

    Fix dark mode flash and set as default theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-08-24", // Current date
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
-  css: ["~/assets/css/tailwind.css"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode" // No longer a dev dependency
+  ],
+  css: ["~/assets/css/tailwind.css"],
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark',  
+    classSuffix: '', 
+  }
 })
